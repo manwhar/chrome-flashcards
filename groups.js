3418b572bab24ede486cb8fe345cf4987954bad4
manu <812194@seq.org>
2025-03-24 13:42:09 -0700

    groups prototype

--- groups.js
+++ groups.js
@@ -1,25 +1,6 @@
 // explicitly defined flashcards
 // once this structure is moved to localstorage, pull from there
 const flashcards = {
-<<<<<<< Updated upstream
-    "MATH": [
-        {"back": "y=mx+b", "front": "point-slope form"},
-        {"back": "9+10", "front": "21"},
-        {"back": "x^2 + y^2 = z^2", "front": "pythagorean theorem"},
-        {"back": "circumference of a circle", "front": "2(pi)r"},
-        {"back": "area of a circle", "front": "pi(r)^2"}
-    ],
-    "SCIENCE": [
-        {"back": "the powerhouse of the cell", "front": "mitochondria"},
-        {"back": "formula for angular velocity", "front": "(1/2)mv^2"},
-        {"back": "what should we always ignore", "front": "air resistance"}
-    ],
-    "HISTORY": [
-        {"who was the first president": "george washington"},
-        {"who wasn't the first president": "katie kubota"},
-        {"idk more history but what is the meaning of life": "42"},
-    ]
-=======
     "MATH": {
         "flashcards": [
             { "back": "y=mx+b", "front": "point-slope form" },
@@ -48,7 +29,6 @@ const flashcards = {
         ],
         "color": "#de57d5"
     }
->>>>>>> Stashed changes
 }
 
 function adjustOpacity(hex, opacity) {
